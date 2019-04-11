225a8bae9b3311a302975e241d53f99493515f73
emerson.fernandes <emerson.fernandes@infraclearsalecom.onmicrosoft.com>
2019-04-11 11:50:18 -0300

    _house

--- src/Program.cs
+++ src/Program.cs
@@ -11,11 +11,11 @@ namespace OCP_Filters
             Console.WriteLine("Hello World!");
             var apple = new Product("Apple", Color.Green, Size.Small);
             var tree = new Product("Tree", Color.Green, Size.Large);
-            var house = new Product("House", Color.Blue, Size.Large);            
+            var _house = new Product("House", Color.Blue, Size.Large);            
 
             //teste 123 456 789        
                                     
-            Product[] products = { apple, tree, house };
+            Product[] products = { apple, tree, _house };
             
             var fs = new FilterService();
             
